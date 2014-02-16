f35659a23a0261c6213ee71e3fc9bb6233b090bf
Brad Gearon <bradgearon@gmail.com>
2014-02-16 03:27:54 -0600

    added event service

---
+++ src/services/events.js
@@ -0,0 +1,17 @@
+define([
+    'app',
+    'hammer'
+],
+    function (app, hammer) {
+        app.service(['events',
+            function () {
+                var Events = function() {
+                    return {
+                        
+                
+                    }
+                };
+                
+            }
+        ]);
+    });
